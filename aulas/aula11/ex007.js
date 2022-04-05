6b2f422312100f58de127476e43388096e0e5db8
Bruno Frohlich <68931062+brunopp00@users.noreply.github.com>
2022-04-05 09:46:34 -0300

    aula11

---
+++ aulas/aula11/ex007.js
@@ -0,0 +1,6 @@
+var vel = 11
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 12) {
+    console.log('Você ultrapassou a velocidade permitida. MUlTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
