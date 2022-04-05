a9b8835e54846d6e0e4aa9b2fb1b7d71b3212fb3
Bruno Frohlich <68931062+brunopp00@users.noreply.github.com>
2022-04-05 11:08:18 -0300

    aula12

---
+++ aulas/aula12/ex011.js
@@ -0,0 +1,12 @@
+var idade = 68
+if (idade < 16) {
+    console.log('Não Vota')
+} 
+else if (idade < 18) {
+        console.log('Voto opcional')
+    } else if (idade >= 67){
+        console.log('Vota opcional')
+    } else {
+        console.log('Voto obrigatório!')
+    }
+
