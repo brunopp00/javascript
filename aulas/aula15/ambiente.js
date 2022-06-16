dfb679b4b72bdc4d11c545a3a4fd532b7340fb74
Bruno Frohlich <68931062+brunopp00@users.noreply.github.com>
2022-06-16 13:22:01 -0300

    aa

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,9 @@
+/*let num = [9, 5, 12]
+num.sort()
+num.push(7)
+
+console.log(`Nosso valor é o ${num}`)*/
+let num = [9, 2, 10, 23]
+for (let pos = 0; pos < num.length; pos++) {
+  console.log(num[pos])
+}
